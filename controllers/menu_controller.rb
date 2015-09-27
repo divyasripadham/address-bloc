97c0d98fcce070fd74d6916c3b6b62f8e539fb90
divyasripadham <divya_sri16@yahoo.com>
2015-09-27 18:38:34 -0400

    Assignment 25 completed, Address Bloc Menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - Demolish"
+     puts "6 - Exit"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -37,6 +38,10 @@ class MenuController
        read_csv
        main_menu
      when 5
+       system "clear"
+       demolish
+       main_menu
+     when 6
        puts "Good-bye!"
        exit(0)
      else
@@ -44,6 +49,10 @@ class MenuController
        puts "Sorry, that is not a valid input"
        main_menu
      end
+   end
+
+   def demolish
+     @address_book.entries.clear
    end
 
    def view_all_entries
